aca48e25c032a21e16f2519c5bfa87a5be55b8e4
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-02-08 16:49:26 +0100

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -8,7 +8,7 @@ leaderboard_answers = user_manager.get_leaderboard_answers(limit=3)
 leaderboard_xp = user_manager.get_leaderboard_xp(limit=3)
 tab1, tab2 = st.tabs(tabs=["Classement par nombre de bonnes réponses", "Classement par nombre de Points d'Experience 🔥"])
 
-with tab1:
+if tab1:
     if leaderboard_answers:
         st.subheader("Classements des joueurs(euses) qui ont le plus de bonnes réponse :")
 
@@ -24,7 +24,7 @@ with tab1:
     else:
         st.write("Personne n'a encore gagné de points ! 🚀")
 
-with tab1:
+elif tab1:
     if leaderboard_xp:
 
         st.subheader("Classements des joueurs(euses) qui ont le plus de Points d'Experience 🔥 :")
