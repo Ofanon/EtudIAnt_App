e5ba338b7dc92e67f968f3300f315d59153a2f09
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-02-08 12:47:16 +0100

    Update questions_user.py

--- questions_user.py
+++ questions_user.py
@@ -9,6 +9,7 @@ main_pages = [st.Page("main.py", title="🏠 Accueil"), st.Page("shop.py", title
 if "started_questions" not in st.session_state:
     with open("Robot.json", "r") as f:
         st.session_state.robot = json.load(f)
+        st_lottie(st.session_state.robot)
     st.session_state.started_questions = True
     st.session_state.questions_user_count = 0
     st.session_state.responses_user = {}
@@ -78,6 +79,3 @@ with col1:
         if st.button("Revenir en arrière", type="tertiary"):
             st.session_state.questions_user_count -= 1
             st.rerun()
-
-with col2:
-    st_lottie(st.session_state.robot)
