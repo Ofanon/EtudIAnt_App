fc16408534686dc6ca544e86e9aaa393aa5a2c6d
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-02-09 19:44:52 +0100

    update

--- leaderboard.py
+++ leaderboard.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import user_manager
+import json
+from streamlit_lottie import st_lottie
 
 st.title("🏆 Top 3 Leaderboard")
 
@@ -7,36 +9,44 @@ st.title("🏆 Top 3 Leaderboard")
 leaderboard_answers = user_manager.get_leaderboard_answers(limit=3)
 leaderboard_xp = user_manager.get_leaderboard_xp(limit=3)
 tab1, tab2 = st.tabs(tabs=["Classement par nombre de bonnes réponses", "Classement par nombre de Points d'Experience 🔥"])
+col1, col2 = st.columns(2)
 
-with tab1:
-    if leaderboard_answers:
-        st.subheader("Classements des joueurs(euses) qui ont le plus de bonnes réponse :")
+if "robot_leaderboard" not in st.session_state:
+    with open("robot_leaderboard.json","r") as f:
+        st.session_state.robot_leaderboard = json.load(f)
+with col1:
+    with tab1:
+        if leaderboard_answers:
+            st.subheader("Classements des joueurs(euses) qui ont le plus de bonnes réponse :")
 
-        for i, (user_id, corrects_answers) in enumerate(leaderboard_answers, start=1):
-            if i == 1:
-                st.write(f"🥇 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
-            elif i == 2:
-                st.write(f"🥈 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
-            elif i == 3:
-                st.write(f"🥉 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
-            else:
-                st.write(f"✨ {i}. {user_id} - {corrects_answers} bonnes réponses")
-    else:
-        st.write("Personne n'a encore gagné de points ! 🚀")
+            for i, (user_id, corrects_answers) in enumerate(leaderboard_answers, start=1):
+                if i == 1:
+                    st.write(f"🥇 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
+                elif i == 2:
+                    st.write(f"🥈 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
+                elif i == 3:
+                    st.write(f"🥉 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
+                else:
+                    st.write(f"✨ {i}. {user_id} - {corrects_answers} bonnes réponses")
+        else:
+            st.write("Personne n'a encore gagné de points ! 🚀")
 
-with tab2:
-    if leaderboard_xp:
+    with tab2:
+        if leaderboard_xp:
 
-        st.subheader("Classements des joueurs(euses) qui ont le plus de Points d'Experience 🔥 :")
+            st.subheader("Classements des joueurs(euses) qui ont le plus de Points d'Experience 🔥 :")
 
-        for i, (user_id, corrects_answers) in enumerate(leaderboard_xp, start=1):
-            if i == 1:
-                st.write(f"🥇 **{i}. {user_id}** - {corrects_answers} 🔥")
-            elif i == 2:
-                st.write(f"🥈 **{i}. {user_id}** - {corrects_answers} 🔥")
-            elif i == 3:
-                st.write(f"🥉 **{i}. {user_id}** - {corrects_answers} 🔥")
-            else:
-                st.write(f"✨ {i}. {user_id} - {corrects_answers} 🔥")
-    else:
-        st.write("Personne n'a encore gagné de points ! 🚀")
+            for i, (user_id, corrects_answers) in enumerate(leaderboard_xp, start=1):
+                if i == 1:
+                    st.write(f"🥇 **{i}. {user_id}** - {corrects_answers} 🔥")
+                elif i == 2:
+                    st.write(f"🥈 **{i}. {user_id}** - {corrects_answers} 🔥")
+                elif i == 3:
+                    st.write(f"🥉 **{i}. {user_id}** - {corrects_answers} 🔥")
+                else:
+                    st.write(f"✨ {i}. {user_id} - {corrects_answers} 🔥")
+        else:
+            st.write("Personne n'a encore gagné de points ! 🚀")
+    
+with col2:
+    st_lottie(st.session_state.robot_leaderboard, height=400, key="robot_leaderboard", loop=True)
--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import json
+
+if "robot_main" not in st.session_state:
+    with open("robot_main.json","r") as f:
+        st.session_state.robot_main = json.load(f)
 
 st.title("📚 Bienvenue dans **l'EtudIAnt** ! 🤖")
 
-st.subheader("🎯 Comment ça marche ?")
-st.markdown("""
-- **L'EtudIAnt** est une **Intelligence Artificielle** basée sur le modèle d'IA de Google.
-- **Gagne des Points d'Expérience** 🔥 en utilisant les services et achète des **Étoiles** ⭐.
-- **Les Étoiles** ⭐ te permettent d'utiliser l'IA plus longtemps !
-- 🎁 **Chaque jour, tu reçois 3 Étoiles gratuitement dans la 🛒 Boutique !**
-- 🏆 **Classement** : Fais partie des meilleurs en accumulant des points d'expérience !
-""")
+col1, col2 = st.columns(2)
 
-st.success("Bon apprentissage et amuse-toi bien ! 🎉")
+with col1:
+    st.subheader("🎯 Comment ça marche ?")
+    st.markdown("""
+    - **L'EtudIAnt** est une **Intelligence Artificielle** basée sur le modèle d'IA de Google.
+    - **Gagne des Points d'Expérience** 🔥 en utilisant les services et achète des **Étoiles** ⭐.
+    - **Les Étoiles** ⭐ te permettent d'utiliser l'IA plus longtemps !
+    - 🎁 **Chaque jour, tu reçois 3 Étoiles gratuitement dans la 🛒 Boutique !**
+    - 🏆 **Classement** : Fais partie des meilleurs en accumulant des points d'expérience !
+    """)
+
+    st.success("Bon apprentissage et amuse-toi bien ! 🎉")
+
+with col2:
+    st_lottie(st.session_state.robot_main, height=400, key="robot_main", loop=True)
