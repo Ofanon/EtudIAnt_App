7b1e39f9f21b516c2a6b4a27d919719e9eb37890
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-02-09 19:46:11 +0100

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -3,50 +3,50 @@ import user_manager
 import json
 from streamlit_lottie import st_lottie
 
-st.title("🏆 Top 3 Leaderboard")
+col1, col2 = st.columns(2)
+with col1:
+    st.title("🏆 Top 3 Leaderboard")
+with col2:
+    st_lottie(st.session_state.robot_leaderboard, height=400, key="robot_leaderboard", loop=True)
 
 # Récupérer le top 5
 leaderboard_answers = user_manager.get_leaderboard_answers(limit=3)
 leaderboard_xp = user_manager.get_leaderboard_xp(limit=3)
 tab1, tab2 = st.tabs(tabs=["Classement par nombre de bonnes réponses", "Classement par nombre de Points d'Experience 🔥"])
-col1, col2 = st.columns(2)
 
 if "robot_leaderboard" not in st.session_state:
     with open("robot_leaderboard.json","r") as f:
         st.session_state.robot_leaderboard = json.load(f)
-with col1:
-    with tab1:
-        if leaderboard_answers:
-            st.subheader("Classements des joueurs(euses) qui ont le plus de bonnes réponse :")
 
-            for i, (user_id, corrects_answers) in enumerate(leaderboard_answers, start=1):
-                if i == 1:
-                    st.write(f"🥇 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
-                elif i == 2:
-                    st.write(f"🥈 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
-                elif i == 3:
-                    st.write(f"🥉 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
-                else:
-                    st.write(f"✨ {i}. {user_id} - {corrects_answers} bonnes réponses")
-        else:
-            st.write("Personne n'a encore gagné de points ! 🚀")
+with tab1:
+    if leaderboard_answers:
+        st.subheader("Classements des joueurs(euses) qui ont le plus de bonnes réponse :")
 
-    with tab2:
-        if leaderboard_xp:
+        for i, (user_id, corrects_answers) in enumerate(leaderboard_answers, start=1):
+            if i == 1:
+                st.write(f"🥇 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
+            elif i == 2:
+                st.write(f"🥈 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
+            elif i == 3:
+                st.write(f"🥉 **{i}. {user_id}** - {corrects_answers} bonnes réponses")
+            else:
+                st.write(f"✨ {i}. {user_id} - {corrects_answers} bonnes réponses")
+    else:
+        st.write("Personne n'a encore gagné de points ! 🚀")
 
-            st.subheader("Classements des joueurs(euses) qui ont le plus de Points d'Experience 🔥 :")
+with tab2:
+    if leaderboard_xp:
 
-            for i, (user_id, corrects_answers) in enumerate(leaderboard_xp, start=1):
-                if i == 1:
+        st.subheader("Classements des joueurs(euses) qui ont le plus de Points d'Experience 🔥 :")
+
+        for i, (user_id, corrects_answers) in enumerate(leaderboard_xp, start=1):
+            if i == 1:
                     st.write(f"🥇 **{i}. {user_id}** - {corrects_answers} 🔥")
-                elif i == 2:
+            elif i == 2:
                     st.write(f"🥈 **{i}. {user_id}** - {corrects_answers} 🔥")
-                elif i == 3:
+            elif i == 3:
                     st.write(f"🥉 **{i}. {user_id}** - {corrects_answers} 🔥")
-                else:
+            else:
                     st.write(f"✨ {i}. {user_id} - {corrects_answers} 🔥")
-        else:
-            st.write("Personne n'a encore gagné de points ! 🚀")
-    
-with col2:
-    st_lottie(st.session_state.robot_leaderboard, height=400, key="robot_leaderboard", loop=True)
+    else:
+        st.write("Personne n'a encore gagné de points ! 🚀")
