f5bd2f04e8f1541f53bfb7db011917c174b70bf3
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-02-12 17:22:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if "robot_main" not in st.session_state:
     with open("robot_main.json","r") as f:
         st.session_state.robot_main = json.load(f)
 
-st.title(f"Bienvenue sur l'EtudIAnt **{st.session_state.user_id}** !")
+st.title(f"🤖 Bienvenue sur l'EtudIAnt **{st.session_state.user_id}** !")
 
 col1, col2 = st.columns(2)
 
