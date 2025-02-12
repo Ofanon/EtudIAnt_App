a7b0766a47a723ee931fe104bc032edf6d5658dc
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-02-12 16:51:21 +0100

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -5,7 +5,6 @@ from streamlit_lottie import st_lottie
 
 st.title("🤖 Connexion à l'EtudIAnt")
 
-st.error("⚠️ Tous les identifiants ont été reinitialisés pour une meilleure gestion des utilisateurs. Crée un compte pour utiliser l'EtudIAnt.")
 if "connexion_type" not in st.session_state:
     st.session_state.connexion_type = "connect"
     st.session_state.user_id = None
