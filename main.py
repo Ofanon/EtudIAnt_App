0c823595802930bd1e8092959f0d6436f020b1a4
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-02-13 18:14:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.title(f"🤖 Bienvenue sur l'EtudIAnt !")
 with st.container(border=True):
     st.subheader("Kaimana part déjà ! Donne-lui des Points d'Experience !")
     xp = st.slider("Combien de Points d'Experience veux-tu lui donner ?", 10, 60)
-    if st.button(f"Donner {xp} à Kaimana"):
+    if st.button(f"Donner {xp} Points d'Experience à Kaimana"):
         user_manager.gift_to_kaimana(user_id=st.session_state.user_id, xp=xp)
         st.balloons()
 col1, col2 = st.columns(2)
