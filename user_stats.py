ef87ad3612664dc56dec0b406f286de7246e8c67
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-02-13 17:54:01 +0100

    Update user_stats.py

--- user_stats.py
+++ user_stats.py
@@ -38,7 +38,7 @@ with st.container(border=True, key="stats_2"):
 
         options = {
             "tooltip": {"trigger": "axis"},
-            "legend": {"data": ["Bonnes Réponses", "Mauvaises Réponses", "Note sur 20"], "selected" : {"Bonnes réponses" : False, "Mauvaises réponses" : False, "Note sur 20" : True}},
+            "legend": {"data": ["Bonnes Réponses", "Mauvaises Réponses", "Note sur 20"], "selected" : {"Bonnes réponses" : True, "Mauvaises réponses" : False, "Note sur 20" : True}},
             "xAxis": {"type": "category", "data": dates},
             "yAxis": {"type": "value"},
             "series": [
