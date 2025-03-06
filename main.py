0e844e7e3c699dafaad3f4486cd8ca8e1369711e
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-03-06 18:46:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,20 +10,6 @@ if "robot_main" not in st.session_state:
 
 st.title(f"🤖 Bienvenue sur l'EtudIAnt !")
 
-with st.container(border=True):
-    st.subheader("**Kaimana part déjà 😭 !**")
-    st.write("**Donne-lui des Points d'Experience 😉 !**")
-    xp = st.slider("Combien de Points d'Experience veux-tu lui donner ?", 10, 60)
-    if st.button(f"**Donner {xp} Points d'Experience à Kaimana**", type="primary"):
-        st.session_state.kaimana_gift = True
-        st.rerun()
-    if st.session_state.kaimana_gift is True:
-        with st.spinner("Les Points d'Experience sont en chemin..."):
-            user_manager.gift_to_kaimana(user_id=st.session_state.user_id, xp=xp)
-            st.balloons()
-            st.session_state.kaimana_gift = False
-    st.write("Les Points d'Experience donnés seront déduits de ton compte.")
-
 col1, col2 = st.columns(2)
 with col2:
     st.subheader("🎯 Comment ça marche ?")
